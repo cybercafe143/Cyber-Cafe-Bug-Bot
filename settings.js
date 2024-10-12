5d74679c58a95dfe84464097c247b49c73d131fe
cybercafe143 <kartikydv143@gmail.com>
2024-10-13 03:20:38 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -10,15 +10,15 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "910000000000"
-global.ownername = "🐛Tech God"
-global.ytname = "YT: Tech-God"
+global.ownernomer = "2348185777508"
+global.ownername = "Peter Parker"
+global.ytname = "Cyber Head "
 global.socialm = "GitHub: techgod143"
 global.location = "India, Up, Rampur"
 
-global.ownernumber = '917466008456'  //creator number
-global.ownername = '🐛Tech god' //owner name
-global.botname = 'Tech God ᵇᵘᵍ ᵇᵒᵗ' //name of the bot
+global.ownernumber = '2348185777508'  //creator number
+global.ownername = '🐛Cyber Head ' //owner name
+global.botname = 'Cyber Cafe ᵇᵘᵍ ᵇᵒᵗ' //name of the bot
 
 //sticker details
 global.packname = 'Sticker By'
